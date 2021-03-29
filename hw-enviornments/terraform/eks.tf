63cde995b566cd2cee8d7699d088e03c26deed40
drew-ludwig <drew.ludwig@broadwing.io>
2021-03-29 12:50:59 -0700

    clean up

--- hw-enviornments/terraform/eks.tf
+++ hw-enviornments/terraform/eks.tf
@@ -2,58 +2,34 @@ locals {
   node_groups = [
     {
       name          = "hw_demo"
-      instance_type = "t3.nano"
+      instance_type = "t3.small"
     }
   ]
-
-  managed_node_groups = [
-    {
-      name          = "managed_base"
-      count         = 1
-      dedicated     = true
-      instance_type = "t3.nano"
-    }
-  ]
-
-#   users = [
-#     {
-#       userarn  = "arn:aws:iam::062791981969:user/taylorludwig"
-#       username = "taylorludwig"
-#       groups   = ["system:masters"]
-#     }
-#   ]
 }
 
-# module "eks" {
-#   source = "github.com/broadwing/terraform-eks.git?ref=v1.2.0"
+module "eks" {
+  source = "github.com/broadwing/terraform-eks.git?ref=v1.2.0"
 
-#   name        = "main"
-#   environment = "prod"
+  name        = "hello_world"
+  environment = "prod"
 
-#   cluster_version = "1.16"
+  cluster_version = "1.16"
 
-#   vpc_id  = module.vpc.vpc_id
-#   subnets = module.vpc.private_subnets
+  vpc_id  = module.vpc.vpc_id
+  subnets = module.vpc.private_subnets
 
-#   nodes_additional_security_group_ids = [module.vpc.default_security_group_id]
+  nodes_additional_security_group_ids = [module.vpc.default_security_group_id]
 
-#   aws_profile = "default"
+  aws_profile = var.AWS_Profile
 
-#   external_dns_domain_filters = ["broadwing.io"]
+  nodes_key_name = "eks"
 
-#   nodes_key_name = "eks"
+  node_groups         = local.node_groups
 
-#   node_groups         = local.node_groups
-#   managed_node_groups = local.managed_node_groups
-
-#   alb_prefix                   = "k8s"
-#   alb_ingress_controller_image = "docker.io/m00nf1sh/aws-alb-ingress-controller:v1.2.0-alpha.2"
-#   get_dashboard_token          = "false"
-
-#   flux                     = true
-#   flux_git_url             = "git@github.com:broadwing/bw_infrastructure.git"
-#   flux_git_path            = "kustomize"
-#   flux_manifest_generation = true
-
-
-# }
+  alb_prefix                   = "k8s"
+  alb_ingress_controller_image = "docker.io/m00nf1sh/aws-alb-ingress-controller:v1.2.0-alpha.2"
+  get_dashboard_token          = "false"
+ 
+  # Get around flux errors even when not installing flux
+  flux_git_url = ""
+}
